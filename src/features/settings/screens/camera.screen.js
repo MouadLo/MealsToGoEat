6f62c7f02404435bcd12574ed7f0cf356e0943b5
Mouad Lousimi <mouad.lousimi@etud.iga.ac.ma>
2022-02-10 06:53:38 +0100

    setup camera module for a Profile Image

--- src/features/settings/screens/camera.screen.js
+++ src/features/settings/screens/camera.screen.js
@@ -1,11 +1,56 @@
-import { View, Text } from "react-native";
-import React from "react";
+import React, { useState, useEffect, useRef, useContext } from "react";
+import { TouchableOpacity, View } from "react-native";
+import { Camera } from "expo-camera";
+import styled from "styled-components/native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Text } from "../../../components/typography/text.component";
+import { AuthenticationContext } from "../../../services/authentication/authentication.context";
 
-const CameraScreen = () => {
+const ProfileCamera = styled(Camera)`
+  width: 100%;
+  height: 100%;
+`;
+
+const CameraScreen = ({ navigation }) => {
+  const [hasPermission, setHasPermission] = useState(null);
+  const cameraRef = useRef();
+
+  const { user } = useContext(AuthenticationContext);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  const snap = async () => {
+    if (cameraRef) {
+      const photo = await cameraRef.current.takePictureAsync();
+
+      await AsyncStorage.setItem(`${user.uid}-photo`, photo.uri);
+      navigation.goBack();
+    }
+  };
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
-    <View>
-      <Text>Camera</Text>
-    </View>
+    <ProfileCamera
+      ref={(camera) => (cameraRef.current = camera)}
+      type={Camera.Constants.Type.front}
+    >
+      <TouchableOpacity
+        style={{ height: "100%", width: "100%" }}
+        onPress={snap}
+      ></TouchableOpacity>
+    </ProfileCamera>
   );
 };
 
--- src/features/settings/screens/settings.screen.js
+++ src/features/settings/screens/settings.screen.js
@@ -1,8 +1,9 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { TouchableOpacity } from "react-native";
 import styled from "styled-components/native";
-
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { List, Avatar } from "react-native-paper";
+import { useFocusEffect } from "@react-navigation/native";
 
 import { Text } from "../../../components/typography/text.component";
 import { Spacer } from "../../../components/spacer/spacer.component";
@@ -26,21 +27,42 @@ const SettingsItem = styled(List.Item)`
   background-color: rgba(255, 255, 255, 0.4);
 `;
 const AvatarContainer = styled.View`
+  margin-top: 15px;
   align-items: center;
 `;
 
 export const SettingsScreen = ({ navigation }) => {
   const { onLogout, user } = useContext(AuthenticationContext);
+  const [photo, setPhoto] = useState(null);
+
+  const getProfilePicture = async (currentUser) => {
+    const photoUri = await AsyncStorage.getItem(`${currentUser.uid}-photo`);
+
+    setPhoto(photoUri);
+  };
+
+  useFocusEffect(() => {
+    getProfilePicture(user);
+  });
+
   return (
     <SettingsBackground>
       <TransparentSafeArea>
         <AvatarContainer>
           <TouchableOpacity onPress={() => navigation.navigate("Camera")}>
-            <Avatar.Icon
-              size={180}
-              icon="human"
-              backgroundColor={colors.brand.primary}
-            />
+            {!photo ? (
+              <Avatar.Icon
+                size={180}
+                icon="human"
+                backgroundColor={colors.brand.primary}
+              />
+            ) : (
+              <Avatar.Image
+                size={180}
+                source={{ uri: photo }}
+                backgroundColor={colors.brand.primary}
+              />
+            )}
           </TouchableOpacity>
           <Spacer position="top" size="large">
             <Text variant="label">{user.email}</Text>
